dd083f7b475844196cd20c68a65fbd3a8026349e
Dawid J. Blom <dawidjblom@gmail.com>
2025-02-18 14:04:34 -0500

    feature: Updates project documentation
    - updated invoice layout.
    - updated statement layout.
    - updated dependency graph generation.
    - adds doxygen installation to the install scripts.
    - updated .gitignore to ignore the unrelated doxygen files.
    
    Signed-off-by: Dawid J. Blom <dawidjblom@gmail.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -40,7 +40,7 @@ function run_install()
 function install_develop()
 {
         dnf_packages=(git cloc lcov gcovr make cmake cppcheck dh-autoreconf automake autoconf gtk4 gtk4-devel
-                      libcurl libcurl-devel json-devel flatpak poppler-cpp-devel)
+                      libcurl libcurl-devel json-devel flatpak poppler-cpp-devel doxygen)
         apt_packages=(git cloc lcov gcovr make cmake cppcheck dh-autoreconf automake autoconf libgtk-4-1 
                       libgtk-4-dev gtk-4-examples flatpak gnome-software-plugin-flatpak nlohmann-json3-dev
                       Libcurl-dev libcurl libpoppler-cpp-dev)
