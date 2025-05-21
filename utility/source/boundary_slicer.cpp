0cd104e5b0a6123f47be004a871145e8eb3f6c60
Dawid J. Blom <dawidjblom@gmail.com>
2025-05-21 07:13:53 -0400

    refactor: Refactored for readability
    Signed-off-by: Dawid J. Blom <dawidjblom@gmail.com>

--- utility/source/boundary_slicer.cpp
+++ utility/source/boundary_slicer.cpp
@@ -3,9 +3,10 @@
  * Author: Dawid J. Blom
  * Date: December 28, 2024
  *
- * NOTE:
+ * NOTE: This utility handles the splitting of words or
+ * the placing of words on a new line depending on their
+ * length. 
  *******************************************************/
-#include <syslog.h>
 #include <boundary_slicer.h>
 
 
@@ -13,48 +14,48 @@ utility::boundary_slicer::~boundary_slicer() {}
 
 std::vector<std::string> utility::boundary_slicer::slice(const std::string& _data)
 {
-        std::vector<std::string> sliced_data{};
-        this->word.clear();
+        std::string word{};
+        this->sliced_data.clear();
         this->current_line.clear();
         if (!_data.empty())
         {
                 std::istringstream stream{_data};
-                while (stream >> this->word)
+                while (stream >> word)
                 {
-                        if (word_does_not_exceed_max())
+                        if (word_does_not_exceed_max(word))
                         {
-                                add_word_to_current_line();
+                                add_word_to_current_line(word);
                         }
-                        else if (word_does_exceed_max())
+                        else if (word_does_exceed_max(word))
                         {
-                                handle_large_word(sliced_data);
+                                handle_large_word(word);
                         }
                         else
                         {
-                                add_new_current_line(sliced_data);
+                                add_new_current_line(word);
                         }
                 }
-                add_whats_left_over(sliced_data);
+                add_whats_left_over();
         }
 
-        return sliced_data;
-} //GCOVR_EXCL_LINE
+        return this->sliced_data;
+} 
 
-bool utility::boundary_slicer::word_does_not_exceed_max()
+bool utility::boundary_slicer::word_does_not_exceed_max(const std::string& _word)
 {
         std::lock_guard<std::mutex> guard(this->data_mutex);
         return (this->current_line.size() +
-                        this->word.size() +
+                        _word.size() +
                         (this->current_line.empty() ?
                          DECISION::NO : DECISION::YES) <= this->max);
 }
 
-bool utility::boundary_slicer::word_does_exceed_max()
+bool utility::boundary_slicer::word_does_exceed_max(const std::string& _word)
 {
-        return (this->word.size() >= this->max);
+        return (_word.size() >= this->max);
 }
 
-void utility::boundary_slicer::add_word_to_current_line()
+void utility::boundary_slicer::add_word_to_current_line(const std::string& _word)
 {
         std::lock_guard<std::mutex> guard(this->data_mutex);
         if (!this->current_line.empty())
@@ -62,28 +63,28 @@ void utility::boundary_slicer::add_word_to_current_line()
                 this->current_line += " ";
         }
 
-        this->current_line += this->word;
+        this->current_line += _word;
 }
 
-void utility::boundary_slicer::handle_large_word(std::vector<std::string>& _data)
+void utility::boundary_slicer::handle_large_word(const std::string& _word)
 {
         size_t pos{0};
-        while (pos < this->word.size()) 
+        while (pos < _word.size()) 
         {
-                size_t len = std::min(this->max, this->word.size() - pos);
-                _data.emplace_back(this->word.substr(pos, len));
+                size_t len = std::min(this->max, _word.size() - pos);
+                this->sliced_data.emplace_back(_word.substr(pos, len));
                 pos += len;
         }
 }
 
-void utility::boundary_slicer::add_new_current_line(std::vector<std::string>& _data)
+void utility::boundary_slicer::add_new_current_line(const std::string& _word)
 {
         std::lock_guard<std::mutex> guard(this->data_mutex);
-        _data.push_back(this->current_line);
-        this->current_line = this->word;
+        this->sliced_data.push_back(this->current_line);
+        this->current_line = _word;
 }
 
-void utility::boundary_slicer::add_whats_left_over(std::vector<std::string>& _data)
+void utility::boundary_slicer::add_whats_left_over()
 {
-        _data.push_back(this->current_line);
+        this->sliced_data.push_back(this->current_line);
 }
