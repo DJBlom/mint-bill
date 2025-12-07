a3c5a91c51c328e5d89a698370a547f62c495d2c
Dawid J. Blom <dawidjblom@gmail.com>
2025-12-07 00:21:53 -0500

    code: Added install
    Signed-off-by: Dawid J. Blom <dawidjblom@gmail.com>

--- app/include/config.h
+++ app/include/config.h
@@ -37,7 +37,6 @@ namespace app {
 namespace config {
 	constexpr int password_number{0};
 	constexpr const char *password_manager_schema_name{"org.mint-bill.password"};
-	// constexpr const char *app_name{APP_ID};
 	constexpr const char *path_to_ui_file{"gui/mint-bill.ui"};
 	constexpr const char *path_to_database_file{"mint-bill.db"};
 }
